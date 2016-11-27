66736a0a5127d4f83a141439850bddb16762514a
Jakub Misiura <jakubmi9@gmail.com>
2016-11-27 23:41:45 +0100

    fixed XML comments

--- cpp-shell/cpp-shell.cpp
+++ cpp-shell/cpp-shell.cpp
@@ -2,9 +2,6 @@
 #include <iostream>
 #include <vector>
 //==============================================================================
-///<summary>initializes new shell object</summary>
-///<param name=_g>greeting message for your program, shows only once upon initialization</param>
-///<param name=_p>prompt message for your program, shows at the beginning of every line and it is followed by a $ symbol</param>
 sh::shell::shell(std::string _g, std::string _p)
 {
 	this->_greet = _g;
@@ -14,11 +11,11 @@ sh::shell::shell(std::string _g, std::string _p)
 	this->_ps2prompt = "> ";
 	std::cout << this->_greet << std::endl;
 }
+//==============================================================================
 sh::shell::~shell()
 {
 }
-///<summary>starts primary shell that awaits user input. That input can be found in this->command</summary>
-///<remarks>should be run in an endless loop, if user does not provide enough input, substart() should be used to get the additional input</remarks>
+//==============================================================================
 void sh::shell::start()
 {
 	std::cout << this->_ps1prompt;
@@ -44,8 +41,7 @@ void sh::shell::start()
 			this->_arg.append(1, this->_c);
 	}
 }
-///<summary>starts secondary shell that awaits user input. Is used for additional input. Input from this shell is appended to this->command, and does not replace its contents</summary>
-///<remarks>should be run in an endless loop</remarks>
+//==============================================================================
 void sh::shell::substart()
 {
 	std::cout << this->_ps2prompt;
--- cpp-shell/cpp-shell.h
+++ cpp-shell/cpp-shell.h
@@ -1,8 +1,10 @@
 #pragma once
 #include <string>
 #include <vector>
+//==============================================================================
 namespace sh
 {
+	///<summary>shell class provides a complete user interface resembling a unix bash. object of this class should be created at the start of your program, it will provide a starting message.</summary>
 	class shell
 	{
 	private:
@@ -11,13 +13,19 @@ namespace sh
 		std::string _ps2prompt;
 		std::string _arg;
 		char _c;
-		bool _done = 1;
 	protected:
 	public:
+		///<summary>initializes new shell object with a greeting message and a prompt symbol</summary>
+		///<param name="greet">greeting message for your program, shows only once upon initialization</param>
+		///<param name="prompt">prompt message for your program, shows at the beginning of every line and it is followed by a $ symbol</param>
 		shell(std::string greet, std::string prompt);
 		~shell();
+		///<summary>starts primary shell that takes user input. that input can be found in the [std::vector &lt;string&gt; command] property. should be run in an endless loop, if user does not provide enough input, substart() should be used to get the additional input</summary>
 		void start();
+		///<summary>starts secondary shell that awaits user input. it is used for additional input. input from this shell is supposed to be appended to the [std::vector &lt;string&gt; command] property. should be run in an endless loop</summary>
 		void substart();
+		///<summary>contains all the arguments that the user typed in.</summary>
 		std::vector<std::string> command;
 	};
 }
+//==============================================================================
