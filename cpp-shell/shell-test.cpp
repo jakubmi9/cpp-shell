fa9467b80ad5b7ef332d194d51d6c54a89454c46
Jakub Misiura <jakubmi9@gmail.com>
2016-11-27 23:23:01 +0100

    example on how to use cpp-shell to require 3 arguments

--- cpp-shell/shell-test.cpp
+++ cpp-shell/shell-test.cpp
@@ -4,21 +4,35 @@ int main()
 {
 	const std::string greet = "testing program for cpp-shell v0.1";
 	const std::string prompt = "test";
-	sh::shell *main = new sh::shell(greet,prompt);
-	while (true)
+	sh::shell *main = new sh::shell(greet, prompt);
+	while(true)
 	{
 		main->start();
-		if (main->command[0] == "exit")
+		if(main->command[0] == "exit")
 		{
 			delete main;
 			std::system("pause");
 			return 0;
 		}
-		if (main->command[0] == "hot")
+		if(main->command[0] == "list")
 		{
-			while (main->command.size() != 3)
+			while(true)
 			{
-				main->substart();
+				try
+				{
+					if(main->command.at(1) == "users")
+					{
+						if(main->command.at(2) == "nearby")
+						{
+							std::cout << "Nearby users:" << std::endl;
+							break;
+						}
+					}
+				}
+				catch(const std::out_of_range&)
+				{
+					main->substart();
+				}
 			}
 		}
 		main->command.clear();
